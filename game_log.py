92c7c599c651a045722a108f36169324949ea1ca
showyou <showyou41@gmail.com>
2015-06-24 08:09:23 +0900

    add date to game_log.py

--- game_log.py
+++ game_log.py
@@ -1,14 +1,15 @@
 import common
 import random
 import sys
+import datetime
 
-DATES_MAX = 1
+DATES_MAX = 10
 COUNT = 100000
 USERS = 10000
 ITEMS = 10
 COINS = [100, 500, 1000, 2000]
+INITDATE = [2013, 6, 1]
 
-stations = common.load_tsv('stations.tsv')
 class game_log_generator():
     def generate(self):
         dates_max = DATES_MAX
@@ -17,10 +18,12 @@ class game_log_generator():
         items = ITEMS
         for dm in xrange(dates_max):
             for cnt in xrange(count):
+                dt = datetime.date(*INITDATE) + datetime.timedelta(dm)
                 user = random.randint(0, users)
                 item = random.randint(0, items)
                 amount = random.choice(COINS)
-                print '2015/06/29,%d,%d,%d' % (user, item, amount)
+                print '%s,%d,%d,%d' % (dt.isoformat(), user, item, amount)
+
 
 if __name__ == '__main__':
     g = game_log_generator()
